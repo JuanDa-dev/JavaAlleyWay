45e1709e9132b9d88aba68718379a880a29c0104
Juan Anzola <juanquiro23@hotmail.com>
2021-02-21 23:27:54 -0500

    First Commit

---
+++ src/java_videogame_80s/Main.java
@@ -0,0 +1,13 @@
+
+package java_videogame_80s;
+
+
+public class Main {
+
+   
+    public static void main(String[] args) {
+        InterfazPrincipalF inter = new InterfazPrincipalF();
+        inter.setVisible(true);
+    }
+    
+}
