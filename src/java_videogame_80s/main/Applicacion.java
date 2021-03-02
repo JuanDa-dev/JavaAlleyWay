4f6cdcfbf62950d38d1543f68bb7a7b89eec551a
Juan Anzola <juanquiro23@hotmail.com>
2021-03-02 18:49:47 -0500

    Clases organizadas en sus respectivos folders

--- src/java_videogame_80s/main/Applicacion.java
+++ src/java_videogame_80s/main/Applicacion.java
@@ -4,7 +4,7 @@ package java_videogame_80s.main;
 import java_videogame_80s.interfaces.InterfazPrincipalF;
 
 
-public class Main {
+public class Applicacion {
 
    
     public static void main(String[] args) {
