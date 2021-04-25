65efb56dedb2ee22c93a05730e9401ec0ba9b54a
sanyval <shadowgallery12@gmail.com>
2021-04-25 13:09:08 -0500

    se agregaron los poderes por medio de enum

--- src/gamecomponents/Power.java
+++ src/gamecomponents/Power.java
@@ -6,60 +6,20 @@ import java.awt.geom.Ellipse2D;
  *
  * @author Santiago Llerena
  */
-public abstract class Power {
-
-//    private class MoveThread extends Thread {
-//        
-//        Power poder;
-//
-//        public MoveThread(Power poder) {
-//            this.poder = poder;
-//        }
-//        
-//        @Override
-//        public void run() {
-//            while (true) {
-//                poder.x+=dx;
-//                try {
-//                    Thread.sleep(1);
-//                } catch (InterruptedException ex) {
-//                    Logger.getLogger(MoveThread.class.getName()).log(Level.SEVERE, null, ex);
-//                }
-//            }
-//        }
-//
-//    }
+public class Power {
+    
     protected float x, y, dy;
+    final Poder poder;
     final int DIAMETRO = 20;
 //    protected MoveThread hilo;
 //
 
-    public Power(float x, float y) {
+    public Power(float x, float y,Poder poder) {
         this.x = x;
         this.y = y;
         this.dy = 0.5f;
-//        hilo=new MoveThread(this);
-//        hilo.start();
+        this.poder=poder;
     }
-//    
-//    public void iniciar(){
-//        hilo.start();
-//    }
-//    
-//    public void reanudar(){
-//        hilo.resume();
-//    }
-//    
-//    public void parar(){
-//        hilo.stop();
-//    }
-//    
-//    public void suspender(){
-//        hilo.suspend();
-//    }   
-
-    public abstract Ellipse2D getShape();
-
     public float getX() {
         return this.x;
     }
